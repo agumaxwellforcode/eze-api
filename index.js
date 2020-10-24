07de23196ec2fc046ed1e4a561cec8c59322e2b6
Agu Maxwell Chigozie <agumaxwell9@gmail.com>
2020-10-24 12:30:53 +0100

    If added, will Update CORS Restrictions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const routes = require("./routes/routes") // new
-// const base = require("./controllers/import") // new
+const routes = require("./routes/routes") //import routes
+const cors = require('cors')
 
 mongoose
     .connect('mongodb+srv://cTs0vMm2mkhB8V4M:cTs0vMm2mkhB8V4M@cluster0.jcmy4.mongodb.net/eze-phone-shop?retryWrites=true&w=majority', {
@@ -11,7 +11,8 @@ mongoose
 	.then(() => {
 		console.log("Connected")
 		const app = express()
-		app.use(express.json()) // new
+		app.use(express.json())
+		app.use(cors(), routes)
 		app.use("/api", routes)
 		
 
